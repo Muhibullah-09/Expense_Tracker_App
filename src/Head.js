cf772214a08e902e019ab27df038c1f471ac30dd
Muhibullah-09 <mc2840696@gmail.com>
2020-08-04 12:15:05 +0500

    Add company logo

--- src/Head.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import './Head.css';
-
-function Head(){
-    return (
-        <h2 className='pin'>A Expense Tracker App in PKR by Muhibullah Khan Kamali</h2>
-    );
-}
-export default Head;
--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React from 'react'
 export const Header = () => {
   return (
     <h2>
-      Expense Tracker
+      Expense Tracker <p>by EebTech</p>
     </h2>
   )
 }
